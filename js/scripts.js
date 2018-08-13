4ed1e201cb834cbcbf3e1a979d2f25c88be1d87b
Your Name <you@example.com>
2018-08-13 16:12:07 +0300

    Add letter change

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@ function triangleTracker () {
 var a = parseInt(prompt("enter the length"));
 var b = parseInt(prompt("enter the height"));
 var c = parseInt(prompt("enter the hypotenuse"));
-if((a===b) &&(b==c)) {
+if (isNaN(a)===true || isNaN(b)===true || isNaN(c)===true || a<=0 || b<=0 || c<=0) {
+  alert("input a valid number")
+}
+else if((a===b) &&(b==c)) {
   alert("This is an equilateral triangle");
   }
   else if((a===b) || (b===c) || (a===c)) {
